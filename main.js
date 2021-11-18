014bf307103989120b29b25aaf3a0f3a891c17e7
shruti-coder22 <79634442+shruti-coder22@users.noreply.github.com>
2021-11-18 20:55:24 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,78 @@
-var img = "";
-var identify = "";
+var animals = "";
+var street = "";
+var garden = "";
+var farm = "";
+var beach = "";
+
+var object = "";
+var opencanvas = "";
 
 function preload() {
-    img = loadImage("dog_cat.jpg");
+    animals = loadImage("animals.jpg");
+    street = loadImage("Street.jpg");
+    garden = loadImage("garden.jpg");
+    farm = loadImage("farm.jpg");
+    beach = loadImage("beach.jpg");
 }
 
 function setup() {
-    if (identify=="yes") {
-        canvas = createCanvas(640, 420);
-        canvas.center();
-    }
+    canvas = createCanvas(640, 420);
+    canvas.center();
 }
 
 function draw() {
-    if (identify == "yes") {
-        image(img, 0, 0, 640, 420);
     
-        strokeWeight(5);
+    if (opencanvas == "yes") {
+        if (object=="animals"){
+            image(animals, 0, 0, 640, 420);
+    
+            strokeWeight(5);
 
-        fill("#00C6CF");
-        text("Dog", 290, 30);
-        noFill();
-        stroke("#00C6CF");
-        rect(280, 15, 300, 340);
-        noStroke();
+            fill("#BF53FF");
+            text("Elephant", 190, 30);
+            noFill();
+            stroke("#BF53FF");
+            rect(190, 15, 300, 300);
+            noStroke();
 
-        fill("rgb(255, 222, 172)");
-        text("Cat", 70, 100);
-        noFill();
-        stroke("rgb(255, 222, 172)");
-        rect(60, 85, 300, 250);
-        noStroke();
+            fill("rgb(255, 222, 172)");
+            text("Crocodile", 200, 300);
+            noFill();
+            stroke("rgb(255, 222, 172)");
+            rect(190, 280, 300, 250);
+            noStroke();
+
+            fill("#DE0082");
+            text("Shark", 60, 30);
+            noFill();
+            stroke("#DE0082");
+            rect(35, 15, 150, 200);
+            noStroke();
+        }
     }
+}
+
+function pic1() {
+    opencanvas = "yes";
+    object = "animals";
+}
+
+function pic2() {
+    opencanvas = "yes";
+    object = "street";
+}
+
+function pic3() {
+    opencanvas = "yes";
+    object = "garden";
+}
+
+function pic4() {
+    opencanvas = "yes";
+    object = "farm";
+}
+
+function pic5() {
+    opencanvas = "yes";
+    object = "beach";
 }
